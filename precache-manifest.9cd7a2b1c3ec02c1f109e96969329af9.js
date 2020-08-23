21dea19c01714a768ac7d9467eb6239e4c34777e
Alexander Anding <alexander.anding@gmail.com>
2020-08-23 15:43:08 +0200

    Updates

---
+++ precache-manifest.9cd7a2b1c3ec02c1f109e96969329af9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c781cee3710090a26a7d9a7efbaa9df2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a732be8b172d93a16f2f",
+    "url": "/monsters-rolodex/static/css/2.c19b8e0f.chunk.css"
+  },
+  {
+    "revision": "1bb03accc4c3180d85b3",
+    "url": "/monsters-rolodex/static/css/main.f0664760.chunk.css"
+  },
+  {
+    "revision": "a732be8b172d93a16f2f",
+    "url": "/monsters-rolodex/static/js/2.14a470fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.14a470fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb03accc4c3180d85b3",
+    "url": "/monsters-rolodex/static/js/main.0c4fb96b.chunk.js"
+  },
+  {
+    "revision": "a25f4cbc8a8359f7863f",
+    "url": "/monsters-rolodex/static/js/runtime-main.2dd990b0.js"
+  },
+  {
+    "revision": "4c8130da4e923951a97997b32fb315f6",
+    "url": "/monsters-rolodex/static/media/bigelow-rules-latin-400.4c8130da.woff2"
+  },
+  {
+    "revision": "8b43932d9f851245d35a9b3c8b3c6231",
+    "url": "/monsters-rolodex/static/media/bigelow-rules-latin-400.8b43932d.woff"
+  }
+]);
